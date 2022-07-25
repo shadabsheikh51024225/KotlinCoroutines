eba7d8b718c0a2be169a881e3a9089b0abfe6d59
shadabsheikh51024225 <shadabsheikh07@gmail.com>
2022-07-25 17:21:17 -0400

    co-routine context.

---
+++ app/src/test/java/Coroutines/step_15_coroutine_context.kt
@@ -0,0 +1,29 @@
+package Coroutines
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.runBlocking
+
+/**
+ * The coroutine context includes a coroutine dispatcher (see CoroutineDispatcher)
+ * that determines what thread or threads the corresponding coroutine uses for its execution.
+ * The coroutine dispatcher can confine coroutine execution to a specific thread, dispatch it to a thread pool,
+ * or let it run unconfined.
+ *
+ * */
+
+fun main() = runBlocking<Unit> {
+    launch { // context of the parent, main runBlocking coroutine
+        println("main runBlocking      : I'm working in thread ${Thread.currentThread().name}")
+    }
+    launch(Dispatchers.Unconfined) { // not confined -- will work with main thread
+        println("Unconfined            : I'm working in thread ${Thread.currentThread().name}")
+    }
+    launch(Dispatchers.Default) { // will get dispatched to DefaultDispatcher
+        println("Default               : I'm working in thread ${Thread.currentThread().name}")
+    }
+    launch(newSingleThreadContext("MyOwnThread")) { // will get its own new thread
+        println("newSingleThreadContext: I'm working in thread ${Thread.currentThread().name}")
+    }
+}
